965230413261b5ce10f7275f0c8da223e750379f
Marek Cel <marek.cel@gmail.com>
2022-03-28 18:43:32 +0200

    reorganization

---
+++ mcSima/gear/WinchLauncher.cpp
@@ -0,0 +1,124 @@
+/****************************************************************************//*
+ * Copyright (C) 2022 Marek M. Cel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ ******************************************************************************/
+
+#include <mcsim/gear/WinchLauncher.h>
+
+#include <cmath>
+
+#include <mcutil/misc/String.h>
+#include <mcutil/physics/Physics.h>
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace mc
+{
+
+////////////////////////////////////////////////////////////////////////////////
+
+WinchLauncher::WinchLauncher()
+    : _for_max ( 0.0 )
+    , _len_max ( 0.0 )
+    , _ang_max ( 0.0 )
+    , _vel_max ( 0.0 )
+
+    , _tc_for ( 0.0 )
+    , _tc_vel ( 0.0 )
+
+    , _stiffness ( 0.0 )
+
+    , _for ( 0.0 )
+    , _vel ( 0.0 )
+    , _len ( 0.0 )
+
+    , _active ( true )
+{}
+
+////////////////////////////////////////////////////////////////////////////////
+
+WinchLauncher::~WinchLauncher() {}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void WinchLauncher::readData( XmlNode &dataNode )
+{
+    if ( dataNode.isValid() )
+    {
+
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void WinchLauncher::computeForceAndMoment( const Matrix3x3 &wgs2bas,
+                                           const Vector3 &pos_wgs )
+{
+    _for_bas.zeroize();
+    _mom_bas.zeroize();
+
+    if ( _active )
+    {
+        Vector3 pos_bas = wgs2bas * ( _pos_wgs - pos_wgs );
+
+        double x = std::max( 0.0, pos_bas.getLength() - _len );
+
+        _for_bas = std::max( _for, fabs( x ) * x * _stiffness ) * pos_bas.getNormalized();
+        _mom_bas = _r_a_bas % _for_bas;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void WinchLauncher::update( double timeStep,
+                            const Matrix3x3 &bas2wgs,
+                            const Matrix3x3 &wgs2ned,
+                            const Vector3 &pos_wgs,
+                            double altitude_agl )
+{
+    if ( _active )
+    {
+        if ( timeStep > 0.0 )
+        {
+            _for = Physics::inertia( _for_max, _for, timeStep, _tc_for );
+            _vel = Physics::inertia( _vel_max, _vel, timeStep, _tc_vel );
+            _len = _len - timeStep * _vel;
+
+            Vector3 pos_ned = wgs2ned * ( pos_wgs - _pos_wgs );
+
+            if ( atan2( -pos_ned.z(), pos_ned.getLengthXY() ) > _ang_max )
+            {
+                _active = false;
+            }
+        }
+        else
+        {
+            if ( altitude_agl < 0.0 )
+            {
+                _pos_wgs = pos_wgs + bas2wgs * Vector3( _len_max, 0.0, 0.0 );
+                _len = _len_max;
+            }
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // mc
---
+++ mcSima/mcsim.pri
@@ -0,0 +1,85 @@
+HEADERS += \
+    $$PWD/defs.h \
+    $$PWD/doxygen.h
+
+SOURCES += \
+    $$PWD/mcsim.cpp
+
+################################################################################
+
+HEADERS += \
+    $$PWD/aero/AeroAngles.h \
+    $$PWD/aero/Fuselage.h \
+    $$PWD/aero/MAC.h \
+    $$PWD/aero/MainRotor.h \
+    $$PWD/aero/PrandtlGlauert.h \
+    $$PWD/aero/Schrenk.h \
+    $$PWD/aero/SimpleRotor.h \
+    $$PWD/aero/StabilizerHor.h \
+    $$PWD/aero/StabilizerVer.h \
+    $$PWD/aero/TailOff.h \
+    $$PWD/aero/TailRotor.h
+
+SOURCES += \
+    $$PWD/aero/AeroAngles.cpp \
+    $$PWD/aero/Fuselage.cpp \
+    $$PWD/aero/MAC.cpp \
+    $$PWD/aero/MainRotor.cpp \
+    $$PWD/aero/PrandtlGlauert.cpp \
+    $$PWD/aero/Schrenk.cpp \
+    $$PWD/aero/SimpleRotor.cpp \
+    $$PWD/aero/StabilizerHor.cpp \
+    $$PWD/aero/StabilizerVer.cpp \
+    $$PWD/aero/TailOff.cpp \
+    $$PWD/aero/TailRotor.cpp
+
+################################################################################
+
+HEADERS += \
+    $$PWD/ctrl/Channel.h \
+    $$PWD/ctrl/HingeMoment.h
+
+SOURCES += \
+    $$PWD/ctrl/HingeMoment.cpp
+
+################################################################################
+
+HEADERS += \
+    $$PWD/envr/AtmosphereUS76.h
+
+SOURCES += \
+    $$PWD/envr/AtmosphereUS76.cpp
+
+################################################################################
+
+HEADERS += \
+    $$PWD/gear/Wheel.h \
+    $$PWD/gear/WinchLauncher.h \
+    $$PWD/gear/WingRunner.h
+
+SOURCES += \
+    $$PWD/gear/Wheel.cpp \
+    $$PWD/gear/WinchLauncher.cpp \
+    $$PWD/gear/WingRunner.cpp
+
+################################################################################
+
+HEADERS += \
+    $$PWD/mass/InertiaMatrix.h \
+    $$PWD/mass/VariableMass.h
+
+SOURCES += \
+    $$PWD/mass/InertiaMatrix.cpp \
+    $$PWD/mass/VariableMass.cpp
+
+################################################################################
+
+HEADERS += \
+    $$PWD/prop/Governor.h \
+    $$PWD/prop/PistonEngine.h \
+    $$PWD/prop/Propeller.h
+
+SOURCES += \
+    $$PWD/prop/Governor.cpp \
+    $$PWD/prop/PistonEngine.cpp \
+    $$PWD/prop/Propeller.cpp
