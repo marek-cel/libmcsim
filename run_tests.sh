bbf71f752fa90e1b4c32c355f2b613c311baee33
Marek Cel <marek.cel@gmail.com>
2022-03-30 17:54:38 +0200

    tests project file and running scripts

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@
 ################################################################################
 
 cd bin
-./libmcsim_tests --gtest_break_on_failure
+./tests --gtest_break_on_failure
 
 read -p "Press any key to continue... " -n1 -s
 
