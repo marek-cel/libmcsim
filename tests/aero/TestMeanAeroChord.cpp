445d16a3229f6558e849e145abc03cb402f8ba07
Marek Cel <marek.cel@gmail.com>
2022-11-15 17:22:43 +0100

    MAC tests

--- tests/aero/TestMeanAeroChord.cpp
+++ tests/aero/TestMeanAeroChord.cpp
@@ -17,7 +17,78 @@ protected:
 
 ////////////////////////////////////////////////////////////////////////////////
 
-TEST_F(TestMeanAeroChord, CanGetMeanAerodynamicChord)
+TEST_F(TestMeanAeroChord, CanGetMeanAerodynamicChordFromCrAndCt)
 {
-    ASSERT_TRUE( true );
+    double cr = 1.0;
+    double ct = 1.0;
+    double mac = mc::getMeanAerodynamicChord( cr, ct );
+    EXPECT_NEAR( mac, 1.0, 1.0e-3 );
+
+    cr = 2.0;
+    ct = 1.0;
+    mac = mc::getMeanAerodynamicChord( cr, ct );
+    EXPECT_NEAR( mac, 1.5556, 1.0e-3 );
+
+    cr = 1.0;
+    ct = 2.0;
+    mac = mc::getMeanAerodynamicChord( cr, ct );
+    EXPECT_NEAR( mac, 1.5556, 1.0e-3 );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST_F(TestMeanAeroChord, CanGetMeanAerodynamicChordFromTable)
+{
+    std::vector<double> y;
+    std::vector<double> c;
+
+    y.push_back( 0.0 );
+    c.push_back( 1.0 );
+
+    y.push_back( 1.0 );
+    c.push_back( 1.0 );
+
+    double mac = mc::getMeanAerodynamicChord( mc::Table( y, c ) );
+    EXPECT_NEAR( mac, 1.0, 1.0e-3 );
+
+    y.clear();
+    c.clear();
+
+    y.push_back( 0.0 );
+    c.push_back( 2.0 );
+
+    y.push_back( 1.0 );
+    c.push_back( 1.0 );
+
+    mac = mc::getMeanAerodynamicChord( mc::Table( y, c ) );
+    EXPECT_NEAR( mac, 1.5556, 1.0e-3 );
+
+    y.clear();
+    c.clear();
+
+    y.push_back( 0.0 );
+    c.push_back( 1.0 );
+
+    y.push_back( 1.0 );
+    c.push_back( 2.0 );
+
+    mac = mc::getMeanAerodynamicChord( mc::Table( y, c ) );
+    EXPECT_NEAR( mac, 1.5556, 1.0e-3 );
+
+    y.clear();
+    c.clear();
+
+    // https://rcplanes.online/cg2_calc.htm
+
+    y.push_back( 0.0 );
+    c.push_back( 4.0 );
+
+    y.push_back( 1.0 );
+    c.push_back( 2.0 );
+
+    y.push_back( 2.0 );
+    c.push_back( 1.0 );
+
+    mac = mc::getMeanAerodynamicChord( mc::Table( y, c ) );
+    EXPECT_NEAR( mac, 2.59, 1.0e-2 );
 }
