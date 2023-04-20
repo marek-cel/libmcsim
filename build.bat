32a7ad021151cedce5c84935dd59bd5a9d7a27c3
Marek Cel <marek.cel@gmail.com>
2023-04-20 21:58:28 +0200

    windows building script style

--- build.bat
+++ build.bat
@@ -3,5 +3,10 @@ call clean.bat
 mkdir build
 cd build
 
-cmake .. -DCMAKE_BUILD_TYPE=Release -DMCUTILS_INCLUDE_DIR="%LIBMCUTILS_DIR%\include" -DMCUTILS_LIBRARY="%LIBMCUTILS_DIR%\lib\mcutils.lib" -DCMAKE_INSTALL_PREFIX=%LIBMCSIM_DIR%
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DMCUTILS_INCLUDE_DIR="%LIBMCUTILS_DIR%\include" ^
+    -DMCUTILS_LIBRARY="%LIBMCUTILS_DIR%\lib\mcutils.lib" ^
+    -DCMAKE_INSTALL_PREFIX=%LIBMCSIM_DIR%
+
 cmake --build . --config Release
