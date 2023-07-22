39b1d90642c281ed10003a7b91f70458500ac2cc
Marek Cel <marek.cel@gmail.com>
2023-07-22 19:54:27 +0200

    Fix style in TailRotor class

--- mcsim/rotor/TailRotor.cpp
+++ mcsim/rotor/TailRotor.cpp
@@ -31,18 +31,18 @@ namespace mc
 
 ////////////////////////////////////////////////////////////////////////////////
 
-void TailRotor::computeForceAndMoment( const Vector3 &vel_air_bas,
-                                       const Vector3 &omg_air_bas,
-                                       double airDensity )
+void TailRotor::computeForceAndMoment(const Vector3& vel_air_bas,
+                                      const Vector3& omg_air_bas,
+                                      double airDensity)
 {
-    double omega2 = _omega * _omega;
-    double omegaR = _omega * _data.r;
+    double omega2 = omega_ * omega_;
+    double omegaR = omega_ * data_.r;
 
     // velocity transformations
-    Vector3 vel_air_ras = _bas2ras * ( vel_air_bas + ( omg_air_bas % _data.r_hub_bas ) );
+    Vector3 vel_air_ras = bas2ras_ * ( vel_air_bas + ( omg_air_bas % data_.r_hub_bas ) );
 
     // angle of attack
-    double alpha = GetAngleOfAttack( vel_air_ras );
+    double alpha = GetAngleOfAttack(vel_air_ras);
 
     // induced velocity
     double lambda = 0.0;
@@ -50,17 +50,17 @@ void TailRotor::computeForceAndMoment( const Vector3 &vel_air_bas,
     const double airspeed = vel_air_ras.GetLength();
 
     // rotor advance ratio
-    const double mu   = airspeed * cos( alpha ) / omegaR;
+    const double mu   = airspeed * cos(alpha) / omegaR;
     const double mu2  = mu * mu;
-    const double mu_z = airspeed * sin( alpha ) / omegaR;
+    const double mu_z = airspeed * sin(alpha) / omegaR;
 
     // thrust coefficient
     double ct = 0.0;
 
     // rotor inflow
-    double lambda_i = _vel_i / omegaR;
+    double lambda_i = vel_i_ / omegaR;
 
-    if ( fabs( lambda_i ) < 10e-14 ) lambda_i = 10e-14;
+    if ( fabs(lambda_i) < 10e-14 ) lambda_i = 10e-14;
 
     // iteration loopp
     for ( int i = 0; i < 100; ++i )
@@ -68,47 +68,47 @@ void TailRotor::computeForceAndMoment( const Vector3 &vel_air_bas,
         lambda = mu_z - lambda_i;
 
         // thrust coefficient
-        ct = 0.5 * _data.a * _s  * _data.b * ( lambda * _data.b / 2.0 + _theta * ( _data.b + 1.5 * mu2 ) / 3.0 );
-        if ( ct > _data.ct_max ) ct = _data.ct_max;
+        ct = 0.5 * data_.a * s_  * data_.b * ( lambda * data_.b / 2.0 + theta_ * ( data_.b + 1.5 * mu2 ) / 3.0 );
+        if ( ct > data_.ct_max ) ct = data_.ct_max;
 
         // zero function (Padfield p.124)
         double lambda_d = mu2 + lambda * lambda;
-        double g_0 = lambda_i - ct / ( 2.0 * sqrt( lambda_d ) );
+        double g_0 = lambda_i - ct / ( 2.0 * sqrt(lambda_d) );
 
         // break condition
         if ( fabs( g_0 ) < 1.0e-6 ) break;
 
         // (Padfield p.124)
-        double h_j = -( 2.0 * lambda_i * sqrt( lambda_d ) - ct ) * lambda_d
-                / ( 2*pow( lambda_d, 2.0/3.0 ) + _data.a * _s * lambda_d / 4.0 - ct * lambda );
+        double h_j = -( 2.0 * lambda_i * sqrt(lambda_d) - ct ) * lambda_d
+                / ( 2*pow(lambda_d, 2.0/3.0) + data_.a * s_ * lambda_d / 4.0 - ct * lambda );
 
         // (Padfield p.124)
         double f_j = 1.0;
-        if ( fabs( mu_z ) < 0.05 ) f_j = 0.6;
+        if ( fabs(mu_z) < 0.05 ) f_j = 0.6;
 
         // (Padfield p.124)
         double lambda_i_new = lambda_i + f_j * h_j;
 
-        if ( IsValid( lambda_i_new ) ) lambda_i = lambda_i_new;
+        if ( IsValid(lambda_i_new) ) lambda_i = lambda_i_new;
     }
 
     // drag coefficient
-    double cd = _data.delta_0 + _data.delta_2 * ct*ct;
+    double cd = data_.delta_0 + data_.delta_2 * ct*ct;
 
     // moment of resistance coefficient (Bramwell p.102)
-    double cq = cd * _s * ( 1.0 + 3.0 * mu2 ) / 8.0 - lambda * ct;
-    if ( cq > _data.cq_max ) cq = _data.cq_max;
+    double cq = cd * s_ * ( 1.0 + 3.0 * mu2 ) / 8.0 - lambda * ct;
+    if ( cq > data_.cq_max ) cq = data_.cq_max;
 
     // induced velocity
-    _vel_i = lambda_i * omegaR;
+    vel_i_ = lambda_i * omegaR;
 
-    _thrust = _data.thrust_factor * airDensity * _ar * _r2 * omega2 * ct;
-    _torque = _data.torque_factor * airDensity * _ar * _r3 * omega2 * cq;
+    thrust_ = data_.thrust_factor * airDensity * ar_ * r2_ * omega2 * ct;
+    torque_ = data_.torque_factor * airDensity * ar_ * r3_ * omega2 * cq;
 
-    _for_bas = _ras2bas * Vector3( 0.0, 0.0, -_thrust );
-    _mom_bas = _data.r_hub_bas % _for_bas;
+    f_bas_ = ras2bas_ * Vector3(0.0, 0.0, -thrust_);
+    m_bas_ = data_.r_hub_bas % f_bas_;
 
-    if ( !_for_bas.IsValid() || !_mom_bas.IsValid() )
+    if ( !f_bas_.IsValid() || !m_bas_.IsValid() )
     {
         // TODO
     }
@@ -116,10 +116,10 @@ void TailRotor::computeForceAndMoment( const Vector3 &vel_air_bas,
 
 ////////////////////////////////////////////////////////////////////////////////
 
-void TailRotor::update( double omega, double theta )
+void TailRotor::update(double omega, double theta)
 {
-    _omega = omega;
-    _theta = theta;
+    omega_ = omega;
+    theta_ = theta;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- mcsim/rotor/TailRotor.h
+++ mcsim/rotor/TailRotor.h
@@ -57,22 +57,22 @@ public:
     {
         Vector3 r_hub_bas;              ///< [m] rotor hub coordinates expressed in BAS
 
-        int nb { 0 };                   ///< number of rotor blades
+        int nb = 0;                     ///< number of rotor blades
 
-        double r { 0.0 };               ///< [m] rotor radius
-        double c { 0.0 };               ///< [m] blades chord
+        double r = 0.0;                 ///< [m] rotor radius
+        double c = 0.0;                 ///< [m] blades chord
 
-        double a { 0.0 };               ///< [1/rad] blade section lift curve slope
-        double b { 0.0 };               ///< [-] tip losses coefficient
+        double a = 0.0;                 ///< [1/rad] blade section lift curve slope
+        double b = 0.0;                 ///< [-] tip losses coefficient
 
-        double delta_0 { 0.0 };         ///< [-] drag coefficient constant component
-        double delta_2 { 0.0 };         ///< [-] drag coefficient quadratic component
+        double delta_0 = 0.0;           ///< [-] drag coefficient constant component
+        double delta_2 = 0.0;           ///< [-] drag coefficient quadratic component
 
-        double ct_max { DBL_MAX };      ///< [-] maximum thrust coefficient
-        double cq_max { DBL_MAX };      ///< [-] maximum torque coefficient
+        double ct_max = DBL_MAX;        ///< [-] maximum thrust coefficient
+        double cq_max = DBL_MAX;        ///< [-] maximum torque coefficient
 
-        double thrust_factor { 1.0 };   ///< [-] thrust scaling factor
-        double torque_factor { 1.0 };   ///< [-] torque scaling factor
+        double thrust_factor = 1.0;     ///< [-] thrust scaling factor
+        double torque_factor = 1.0;     ///< [-] torque scaling factor
     };
 
     // LCOV_EXCL_START
@@ -90,51 +90,51 @@ public:
      * @param omg_air_bas [rad/s]  aircraft angular velocity relative to the air expressed in BAS
      * @param airDensity  [kg/m^3] air density
      */
-    virtual void computeForceAndMoment( const Vector3 &vel_air_bas,
-                                        const Vector3 &omg_air_bas,
-                                        double airDensity );
+    virtual void computeForceAndMoment(const Vector3& vel_air_bas,
+                                       const Vector3& omg_air_bas,
+                                       double airDensity);
 
     /**
      * @brief Updates rotor model.
      * @param omega      [rad/s] rotor revolution speed
      * @param collective [rad]   collective pitch angle
      */
-    virtual void update( double omega, double collective );
+    virtual void update(double omega, double collective);
 
-    inline const Vector3& getForce_BAS  () const { return _for_bas; }
-    inline const Vector3& getMoment_BAS () const { return _mom_bas; }
+    inline const Vector3& f_bas() const { return f_bas_; }
+    inline const Vector3& m_bas() const { return m_bas_; }
 
-    inline double getRotorInertia() const { return _ir; }
+    inline double ir() const { return ir_; }
 
-    inline double getThrust() const { return _thrust; }
-    inline double getTorque() const { return _torque; }
+    inline double thrust() const { return thrust_; }
+    inline double torque() const { return torque_; }
 
 protected:
 
-    Data _data;                 ///< tail rotor data
+    Data data_;                 ///< tail rotor data
 
-    Vector3 _for_bas;           ///< [N] total force vector expressed in BAS
-    Vector3 _mom_bas;           ///< [N*m] total moment vector expressed in BAS
+    Vector3 f_bas_;             ///< [N] total force vector expressed in BAS
+    Vector3 m_bas_;             ///< [N*m] total moment vector expressed in BAS
 
-    Matrix3x3 _bas2ras;         ///< matrix of rotation from BAS to RAS
-    Matrix3x3 _ras2bas;         ///< matrix of rotation from RAS to BAS
+    Matrix3x3 bas2ras_;         ///< matrix of rotation from BAS to RAS
+    Matrix3x3 ras2bas_;         ///< matrix of rotation from RAS to BAS
 
-    double _r2 { 0.0 };         ///< [m^2] rotor radius squared
-    double _r3 { 0.0 };         ///< [m^3] rotor radius cubed
-    double _ar { 0.0 };         ///< [m^2] rotor disk area
-    double _s  { 0.0 };         ///< [-]   rotor solidity
+    double r2_ = 0.0;           ///< [m^2] rotor radius squared
+    double r3_ = 0.0;           ///< [m^3] rotor radius cubed
+    double ar_ = 0.0;           ///< [m^2] rotor disk area
+    double s_  = 0.0;           ///< [-]   rotor solidity
 
-    double _ib { 0.0 };         ///< [kg*m^2] single rotor blade inertia moment about flapping hinge
-    double _ir { 0.0 };         ///< [kg*m^2] rotor total inertia about shaft axis
+    double ib_ = 0.0;           ///< [kg*m^2] single rotor blade inertia moment about flapping hinge
+    double ir_ = 0.0;           ///< [kg*m^2] rotor total inertia about shaft axis
 
-    double _omega { 0.0 };      ///< [rad/s] rotor revolution speed
+    double omega_ = 0.0;        ///< [rad/s] rotor revolution speed
 
-    double _theta { 0.0 };      ///< [rad] feathering angle
+    double theta_ = 0.0;        ///< [rad] feathering angle
 
-    double _thrust { 0.0 };     ///< [N]   rotor thrust
-    double _torque { 0.0 };     ///< [N*m] rotor torque
+    double thrust_ = 0.0;       ///< [N]   rotor thrust
+    double torque_ = 0.0;       ///< [N*m] rotor torque
 
-    double _vel_i { 0.0 };      ///< [m/s] rotor induced velocity
+    double vel_i_ = 0.0;        ///< [m/s] rotor induced velocity
 };
 
 } // namespace mc
